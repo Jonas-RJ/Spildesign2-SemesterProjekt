4a4b68d08eeac3b3fe6acff1b1ddbf009a9b0b3a
emilowhits <50845354+emilowhits@users.noreply.github.com>
2025-03-26 16:35:58 +0100

    Patrolling og Patrol points
    Har lavet Patrol points prefab og et Patrolling script

---
+++ SemesterProjekt 2 Spildesign/Assets/script/Patrol.cs
@@ -0,0 +1,47 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == patrolPoints[targetPoint].position)
+        {
+            IncreaseTargetInt();
+            StartCoroutine("Stop");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+    }
+
+    void IncreaseTargetInt()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length)
+        {
+            targetPoint = 0;
+        }
+    }
+
+    public IEnumerator Stop()
+    {
+        speed = 0;
+        yield return new WaitForSeconds(5.0f);
+        speed = 4;
+        yield return 0;
+    }
+}
