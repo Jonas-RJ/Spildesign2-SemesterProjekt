47df3cb703133e245f157c4b26f0fededec44076
emilowhits <50845354+emilowhits@users.noreply.github.com>
2025-03-30 01:09:41 +0100

    Comments on Patrol Script

--- SemesterProjekt 2 Spildesign/Assets/script/Patrol.cs
+++ SemesterProjekt 2 Spildesign/Assets/script/Patrol.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 public class Patrol : MonoBehaviour
 {
+    //Setting up Array for Patrolpoints, and variables to access the patrolpoints, speed of the enemy and waiting time on patrol
     public Transform[] patrolPoints;
     public int targetPoint;
     public float speed;
@@ -21,14 +22,23 @@ public class Patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Patrolling();
+    }
+
+    //Method for having the enemy patrol between set points in Array
+    void Patrolling()
+    {
+        //If statement to check if we reached the desired patrolpoint, and cycles to the next point if true
         if (transform.position == patrolPoints[targetPoint].position)
         {
             IncreaseTargetInt();
             StartCoroutine("Stop");
         }
+        //Makes the enemy move towards the patrol points
         transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
     }
 
+    //Method for increasing the targetpoint number and resetting if we reached the threshold
     void IncreaseTargetInt()
     {
         targetPoint++;
@@ -38,6 +48,7 @@ public class Patrol : MonoBehaviour
         }
     }
 
+    //Coroutine for having the enemy stop on it's patrol for x seconds
     public IEnumerator Stop()
     {
         speed = 0;
