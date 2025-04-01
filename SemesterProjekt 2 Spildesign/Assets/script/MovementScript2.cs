4cafc3715f3eb6096d1be6630682f9f676db4c4b
Jonas Ramadan Jepsen 47742720 <jramadanjepsen@gmail.com>
2025-04-01 22:35:25 +0200

    toggle sprint added
    sprint added with OnSprint. Is currently toggle, change to on hold

--- SemesterProjekt 2 Spildesign/Assets/script/MovementScript2.cs
+++ SemesterProjekt 2 Spildesign/Assets/script/MovementScript2.cs
@@ -3,13 +3,15 @@ using UnityEngine.InputSystem;
 
 public class MovementScript2 : MonoBehaviour
 {
+ //  public bool isRunning = Keyboard.current.leftShiftKey.isPressed;
+
 
     [SerializeField] private float moveSpeed = 5f;
     private Vector2 movementInput;
     private Rigidbody rb;
     [SerializeField] CapsuleCollider capsuleCollider;
-
-
+    [SerializeField] private float sprintSpeed = 20f;
+    [SerializeField] private float normalSpeed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,4 +33,29 @@ public class MovementScript2 : MonoBehaviour
         movementInput = direction.Get<Vector2>();
     }
 
+
+    public void OnSprint()
+    {
+     /*  
+        if (isRunning)
+        {moveSpeed = sprintSpeed;
+        }
+        else
+        { moveSpeed = normalSpeed;}
+     */
+
+        if (moveSpeed <= normalSpeed)
+        {
+            print("speed up");
+            moveSpeed += sprintSpeed;
+        }
+        else
+        {
+            print("Slow down");
+            moveSpeed = normalSpeed;
+        }
+
+       
+
+    }
    }
