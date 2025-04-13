1305a1f8af27cceb482594eed9891ea0a34e3e78
Martin Kronborg <martinkronborg@hotmail.dk>
2025-04-13 20:55:12 +0200

    Updated dialogue
    Har opdateret dialog scenen, så nu forsvinder start game knappen, "start your journey" knappen er tilføjet og "Lucas Scene" starter når man er færdig med dialog boksen

--- SemesterProjekt 2 Spildesign/Assets/script/DialogueManager.cs
+++ SemesterProjekt 2 Spildesign/Assets/script/DialogueManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -11,6 +12,8 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI dialogueText;
     
     public Animator animator;
+
+    public GameObject startJourneyButton;
     
     private Queue<string> sentences;                //Keeps track of all the sentences in our dialogue box.
     
@@ -18,6 +21,7 @@ public class DialogueManager : MonoBehaviour
     void Start()
     {
         sentences = new Queue<string>();            //Initializes the queue.
+        startJourneyButton.SetActive(false);
     }
 
     public void StartDialogue(Dialogue dialogue) //This function gets started on the click of the "Start Game" button.
@@ -40,9 +44,13 @@ public class DialogueManager : MonoBehaviour
     {
         if (sentences.Count == 0) //Making sure that there is even more sentences to queue.
         {
-            EndDialogue(); //Ending the dialogue
+            //EndDialogue(); //Ending the dialogue and sliding the canvas off the screen
             
             return;
+        }
+        else if (sentences.Count == 1)
+        {
+            startJourneyButton.SetActive(true);
         }
         string sentence = sentences.Dequeue(); //Gets the next sentence in the queue.
         StopAllCoroutines(); //Stop animating before an eventual new dialogue gets opened. So makes sure that if TypeSentence is already running it stops doing so.
@@ -63,6 +71,11 @@ public class DialogueManager : MonoBehaviour
     void EndDialogue()
     {
         animator.SetBool("IsOpen", false); //Makes sure the dialogue box gets off the screen.
+    }
+
+    public void LoadSceneToStartGame()
+    {
+        SceneManager.LoadScene("Lucas Scene");
     }
     
 }
--- SemesterProjekt 2 Spildesign/Assets/script/DialogueTrigger.cs
+++ SemesterProjekt 2 Spildesign/Assets/script/DialogueTrigger.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.Rendering;
 
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue; //A variable for the dialogue object that we want to have.
+    public GameObject startButton;
     
 
     public void TriggerDialogue()
     {
-        //Finding out DialogueManager and passing in our dialogue object, so that is the dialogue being played
+        //Finding our DialogueManager and passing in our dialogue object, so that is the dialogue being played
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue); //Maybe implement a Singleton pattern here.
+    }
+
+    public void RemoveStartGameButton()
+    {
+        startButton.SetActive(false);
     }
 }
