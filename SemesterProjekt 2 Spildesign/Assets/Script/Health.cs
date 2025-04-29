4e82a8f7cd64a73bac7a379bcfc4cccaaeb873d2
emilowhits <50845354+emilowhits@users.noreply.github.com>
2025-04-29 13:24:26 +0200

    Game Over Screen og health system
    Game over screen og hp system færdig, og de interagere med hinanden

--- SemesterProjekt 2 Spildesign/Assets/Script/Health.cs
+++ SemesterProjekt 2 Spildesign/Assets/Script/Health.cs
@@ -1,14 +1,35 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private int currentHealth;
     [SerializeField] private int maxHealth;
+    [SerializeField] float recovery;
+    public GameObject GameOverScreen;
+    bool CanTakeDamage;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+        CanTakeDamage = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            
+            if (CanTakeDamage)
+            {
+                CanTakeDamage = false;
+                TakeDamage(); 
+                print("damage");
+                Invoke("SetTrue", recovery);
+            }
+        }
     }
 
     void TakeDamage()
@@ -19,7 +40,12 @@ public class Health : MonoBehaviour
         if (currentHealth <= 0)
         {
             //Play captured animation/music
-            //Display Game Over
+            GameOverScreen.SetActive(true);
         }
+    }
+
+    void SetTrue()
+    {
+        CanTakeDamage = true;
     }
 }
