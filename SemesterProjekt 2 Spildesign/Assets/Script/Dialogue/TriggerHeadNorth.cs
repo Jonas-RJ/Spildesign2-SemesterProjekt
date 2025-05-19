a9dc9c75cbbd28dc42bad8bed23d8ce258a624bb
Martin Kronborg <martinkronborg@hotmail.dk>
2025-05-19 13:53:12 +0200

    Head North dialog
    Trust

---
+++ SemesterProjekt 2 Spildesign/Assets/Script/Dialogue/TriggerHeadNorth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class TriggerHeadNorth : MonoBehaviour
+{
+   public GameObject headNorthCanvas;
+   
+   public DialogueTrigger trigger;
+
+
+   public void OnTriggerEnter(Collider other)
+   {
+       if (other.tag == "Player")
+       {
+           trigger.TriggerDialogue();
+           //headNorthCanvas.gameObject.SetActive(true);
+       }
+   }
+
+   public void OnTriggerExit(Collider other)
+   {
+       if (other.tag == "Player")
+       {
+           //headNorthCanvas.gameObject.SetActive(false);
+       }
+   }
+   
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
