6d6d0d9930aa279792acb23d04707d96c514ee64
Enzig-png <Ladingkenny@gmail.com>
2025-05-23 06:01:54 +0200

    character script+huse
    character script og huse

--- SemesterProjekt 2 Spildesign/Assets/Script/main menu scrpit/ViodeoPlayer.cs
+++ SemesterProjekt 2 Spildesign/Assets/Script/main menu scrpit/ViodeoPlayer.cs
@@ -4,26 +4,32 @@ using UnityEngine.Video;
 public class ViodeoPlayer : MonoBehaviour
 {
     public VideoPlayer VideoPlayer;
-
-    public double loopStartTime = 1.0;
-    public double loopEndTime = 1.0;
-
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GetComponent<VideoPlayer>();
-   
+        VideoPlayer.Play();
 
-        VideoPlayer.Play(); 
-        VideoPlayer.isLooping = true;
     }
 
     // Update is called once per frame
-    /*void Update()
+    void Update()
     {
-        if(VideoPlayer.isLooping=true)
+        //videoreplay();
+    }
+
+    /*public void videoreplay()
+    {
+        if (VideoPlayer.loopPointReached) 
         {
-            
+            if (!VideoPlayer.isPlaying)
+            {
+                VideoPlayer.Play();
+                VideoPlayer.time = 4;
+
+            }
         }
+        
     }*/
 }
